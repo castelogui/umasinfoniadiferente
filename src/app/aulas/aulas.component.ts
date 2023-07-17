56ae0de3b99ebda0b8a5ff8ecfe28c4c05590c10
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2023-07-16 20:54:54 -0400

    alter array aulas to object type instrumento

--- src/app/aulas/aulas.component.ts
+++ src/app/aulas/aulas.component.ts
@@ -7,34 +7,47 @@ import { Component, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.Emulated,
 })
 export class AulasComponent {
-  aulas = [
+  instrumentos: instrumento[] = [
     {
+      id: 1,
       name: 'Bateria',
       avatar: '../../assets/instrumentos/bateria.png',
     },
     {
+      id: 2,
       name: 'Violão',
       avatar: '../../assets/instrumentos/violão.png',
     },
     {
+      id: 3,
       name: 'Guitarra',
       avatar: '../../assets/instrumentos/guitarra.png',
     },
     {
+      id: 4,
       name: 'Teclado',
       avatar: '../../assets/instrumentos/teclado.png',
     },
     {
+      id: 5,
       name: 'Cajon',
       avatar: '../../assets/instrumentos/cajon.png',
     },
     {
+      id: 6,
       name: 'Percussão',
       avatar: '../../assets/instrumentos/percussão.png',
     },
     {
+      id: 7,
       name: 'Ukulele',
       avatar: '../../assets/instrumentos/ukulele.png',
     }
   ]
+}
+
+export class instrumento {
+  id: number = 0;
+  name: string = '';
+  avatar: string = '';
 }
