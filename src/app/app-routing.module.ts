add15b8d5d127c9cdd463f17d56c21cfed1e69a8
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2023-07-16 22:04:29 -0400

    add route of subscribe

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SobreComponent } from './sobre/sobre.component';
 import { AulasComponent } from './aulas/aulas.component';
+import { SubscribeComponent } from './subscribe/subscribe.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'sobre', component: SobreComponent },
   { path: 'aulas', component: AulasComponent },
+  { path: 'inscricao', component: SubscribeComponent }
 ];
 
 @NgModule({
