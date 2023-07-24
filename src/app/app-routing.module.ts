93e04383280ec82e05873e6d28c1ef498f12389c
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2023-07-24 01:44:04 -0400

    fix: adjust routes and scroll when changing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,26 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Router, RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SobreComponent } from './sobre/sobre.component';
 import { AulasComponent } from './aulas/aulas.component';
 import { SubscribeComponent } from './subscribe/subscribe.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'sobre', component: SobreComponent },
   { path: 'aulas', component: AulasComponent },
-  { path: 'inscricao', component: SubscribeComponent }
+  { path: 'inscricao', component: SubscribeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      window.scrollTo(0, 0);
+    });
+  }
+}
