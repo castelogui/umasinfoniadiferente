2a87469b1544e85d72a7b837f8e0afc9248b9155
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2023-07-24 00:48:39 -0400

    chore: timeline component modularization

--- src/app/sobre/sobre.component.ts
+++ src/app/sobre/sobre.component.ts
@@ -6,31 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./sobre.component.sass']
 })
 export class SobreComponent {
-  eventos: LinhaDoTempo[] = [
-    {
-      ano: 2010,
-      name: 'Evento 1',
-      descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl quis ultricies ultrices, nunc nisl ultr',
-      img: '../../assets/eventos/evento1.jpg',
-    },
-    {
-      ano: 2010,
-      name: 'Evento 1',
-      descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl quis ultricies ultrices, nunc nisl ultr',
-      img: '../../assets/eventos/evento1.jpg',
-    },
-    {
-      ano: 2010,
-      name: 'Evento 1',
-      descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl quis ultricies ultrices, nunc nisl ultr',
-      img: '../../assets/eventos/evento1.jpg',
-    }
-  ]
-}
-
-export class LinhaDoTempo {
-  ano: number = 0;
-  name: string = '';
-  descricao: string = '';
-  img: string = '';
 }
