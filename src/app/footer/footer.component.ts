9062147823c1b614d97476f167e4cb0c10f95303
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2024-03-16 10:08:21 -0400

    adjust footer variables

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.sass']
 })
 export class FooterComponent {
-
+  project = {
+    nome: 'Uma Sinfonia Diferente',
+    local: 'R. Seringueira, 1856 - St. 1, Ariquemes - RO, 76870-142',
+    email: 'projetousd@gmail.com',
+    linkWapp: 'https://api.whatsapp.com/send?phone=556981062874',
+    phone: '(69) 9 8106-2874',
+    insta: 'umasinfoniadiferente',
+    facebook: 'umasinfoniadiferente',
+    creator: 'castelo-guilherme',
+    creatorName: 'Guilherme Castelo'
+  }
 }
