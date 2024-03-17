783e350aa8f2545ea6f9964620931cf4b74c92dc
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2024-03-17 00:55:22 -0400

    update build

--- src/app/services/timeline/timeline.service.ts
+++ src/app/services/timeline/timeline.service.ts
@@ -8,7 +8,7 @@ import { Timeline } from 'src/app/classes/timeline';
 })
 export class TimelineService {
   constructor(private _httpClient: HttpClient) {}
-  private url = 'https://umasinfoniadiferente.vercel.app/history';
+  private url = 'https://api-storage-umasinfoniadiferente.vercel.app/history';
 
   getTimeline(): Observable<Timeline[]> {
     return this._httpClient.get<Timeline[]>(this.url);
