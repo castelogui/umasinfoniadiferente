e7a315c12227fa565295ed30ee742efb35835ef6
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2024-03-22 14:47:18 -0400

    add envirements apiUrl

--- src/app/services/timeline/timeline.service.ts
+++ src/app/services/timeline/timeline.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Timeline } from 'src/app/classes/timeline';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TimelineService {
   constructor(private _httpClient: HttpClient) {}
-  private url = 'https://api-storage-umasinfoniadiferente.vercel.app/history';
+  private url = environment.apiUrl + '/history';
 
   getTimeline(): Observable<Timeline[]> {
     return this._httpClient.get<Timeline[]>(this.url);
--- src/app/services/tutores/tutores.service.ts
+++ src/app/services/tutores/tutores.service.ts
@@ -2,13 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Tutores } from 'src/app/classes/tutores';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TutoresService {
   constructor(private _httpClient: HttpClient) {}
-  private url = 'https://api-storage-umasinfoniadiferente.vercel.app/tutors';
+  private url = environment.apiUrl + '/tutors';
 
   getTutores(): Observable<Tutores[]> {
     return this._httpClient.get<Tutores[]>(this.url);
