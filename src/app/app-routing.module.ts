2d555b0aaa04a288833a722c4209752c8929d0dd
Guilherme Castelo <48875867+castelogui@users.noreply.github.com>
2024-06-02 18:34:54 -0400

    Update app-routing.module.ts: remove timeline

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,13 @@ import { Router, RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AulasComponent } from './pages/aulas/aulas.component';
 import { SubscribeComponent } from './pages/subscribe/subscribe.component';
-import { TimelineComponent } from './pages/timeline/timeline.component';
+//import { TimelineComponent } from './pages/timeline/timeline.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'aulas', component: AulasComponent },
   { path: 'inscricao', component: SubscribeComponent },
-  { path: 'historia', component: TimelineComponent },
+  //{ path: 'historia', component: TimelineComponent },
 ];
 
 @NgModule({
